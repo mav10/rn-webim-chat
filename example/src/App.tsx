fae3bbe9909db5753263bdcb1f0c15b661be6be2
maxim vasin <maxim.vasin@mcc-tomsk.de>
2022-11-16 16:18:58 +0700

    fix: rewrite android project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,32 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'rn-webim-chat';
+import { StyleSheet, Text, View } from 'react-native';
+import { RNWebim, WebimMessage } from 'rn-webim-chat';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [result, setResult] = React.useState<WebimMessage[]>([]);
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    RNWebim.resumeSession({
+      location: 'Tomsk',
+      appVersion: '1.0.0',
+      accountName: 'test',
+    }).then(() => {
+      RNWebim.getAllMessages().then(({ messages }) => setResult(messages));
+    });
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result:messages</Text>
+      {result.map((x) => {
+        return (
+          <View key={x.id} style={styles.message}>
+            <Text>{x.text}</Text>
+            <Text>{x.time}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 }
@@ -27,5 +41,9 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  message: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,23 @@
-import { NativeModules, Platform } from 'react-native';
+import {
+  EmitterSubscription,
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+} from 'react-native';
+import type {
+  DialogClearedListener,
+  ErrorListener,
+  NativeError,
+  NewMessageListener,
+  RemoveMessageListener,
+  SessionBuilderParams,
+  TokenUpdatedListener,
+  UpdateMessageListener,
+  WebimEventListener,
+  WebimMessage,
+} from './types';
+import { WebimEvents } from './types';
+import { processError, WebimSubscription } from './utils';
 
 const LINKING_ERROR =
   `The package 'rn-webim-chat' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,6 +36,181 @@ const RnWebimChat = NativeModules.RnWebimChat
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return RnWebimChat.multiply(a, b);
+const emitter = new NativeEventEmitter(RnWebimChat);
+
+const DEFAULT_MESSAGES_LIMIT = 100;
+
+export class RNWebim {
+  static resumeSession(params: SessionBuilderParams): Promise<void> {
+    return new Promise((resolve, reject) => {
+      RnWebimChat.resumeSession(
+        params,
+        (error: NativeError) => reject(processError(error)),
+        () => resolve()
+      );
+    });
+  }
+
+  static destroySession(clearData: boolean = false) {
+    return new Promise((resolve, reject) => {
+      RnWebimChat.destroySession(
+        clearData,
+        (error: NativeError) => reject(processError(error)),
+        resolve
+      );
+    });
+  }
+
+  static getLastMessages(
+    limit: number = DEFAULT_MESSAGES_LIMIT
+  ): Promise<{ messages: WebimMessage[] }> {
+    return new Promise((resolve, reject) => {
+      RnWebimChat.getLastMessages(
+        limit,
+        (error: NativeError) => reject(processError(error)),
+        (messages: { messages: WebimMessage[] }) => resolve(messages)
+      );
+    });
+  }
+
+  static getNextMessages(
+    limit: number = DEFAULT_MESSAGES_LIMIT
+  ): Promise<{ messages: WebimMessage[] }> {
+    return new Promise((resolve, reject) => {
+      RnWebimChat.getNextMessages(
+        limit,
+        (error: NativeError) => reject(processError(error)),
+        (messages: { messages: WebimMessage[] }) => resolve(messages)
+      );
+    });
+  }
+
+  static getAllMessages(): Promise<{ messages: WebimMessage[] }> {
+    return new Promise((resolve, reject) => {
+      RnWebimChat.getAllMessages(
+        (error: NativeError) => reject(processError(error)),
+        (messages: { messages: WebimMessage[] }) => resolve(messages)
+      );
+    });
+  }
+
+  static send(message: string) {
+    return new Promise((resolve, reject) =>
+      RnWebimChat.send(
+        message,
+        (error: NativeError) => reject(processError(error)),
+        (id: string) => resolve(id)
+      )
+    );
+  }
+
+  static rateOperator(rate: number) {
+    return new Promise((resolve, reject) => {
+      RnWebimChat.rateOperator(
+        rate,
+        (error: NativeError) => reject(processError(error)),
+        resolve
+      );
+    });
+  }
+
+  static tryAttachFile() {
+    return new Promise((resolve, reject) => {
+      RnWebimChat.tryAttachFile(
+        (error: NativeError) => reject(processError(error)),
+        async (file: {
+          uri: string;
+          name: string;
+          mime: string;
+          extension: string;
+        }) => {
+          const { uri, name, mime, extension } = file;
+          try {
+            await RNWebim.sendFile(uri, name, mime, extension);
+            resolve;
+          } catch (e: any) {
+            reject(processError(e));
+          }
+        }
+      );
+    });
+  }
+
+  static sendFile(uri: string, name: string, mime: string, extension: string) {
+    return new Promise((resolve, reject) =>
+      RnWebimChat.sendFile(uri, name, mime, extension, reject, resolve)
+    );
+  }
+
+  public static addNewMessageListener(
+    listener: NewMessageListener
+  ): WebimSubscription {
+    const subscription = emitter.addListener(WebimEvents.NEW_MESSAGE, listener);
+    return new WebimSubscription(() => RNWebim.removeListener(subscription));
+  }
+
+  public static addRemoveMessageListener(
+    listener: RemoveMessageListener
+  ): WebimSubscription {
+    const subscription = emitter.addListener(
+      WebimEvents.REMOVE_MESSAGE,
+      listener
+    );
+    return new WebimSubscription(() => RNWebim.removeListener(subscription));
+  }
+
+  public static addEditMessageListener(
+    listener: UpdateMessageListener
+  ): WebimSubscription {
+    const subscription = emitter.addListener(
+      WebimEvents.EDIT_MESSAGE,
+      listener
+    );
+    return new WebimSubscription(() => RNWebim.removeListener(subscription));
+  }
+
+  public static addDialogClearedListener(
+    listener: DialogClearedListener
+  ): WebimSubscription {
+    const subscription = emitter.addListener(
+      WebimEvents.CLEAR_DIALOG,
+      listener
+    );
+    return new WebimSubscription(() => RNWebim.removeListener(subscription));
+  }
+
+  public static addTokenUpdatedListener(
+    listener: TokenUpdatedListener
+  ): WebimSubscription {
+    const subscription = emitter.addListener(
+      WebimEvents.TOKEN_UPDATED,
+      listener
+    );
+    return new WebimSubscription(() => RNWebim.removeListener(subscription));
+  }
+
+  public static addErrorListener(listener: ErrorListener): WebimSubscription {
+    const subscription = emitter.addListener(WebimEvents.ERROR, listener);
+    return new WebimSubscription(() => RNWebim.removeListener(subscription));
+  }
+
+  public static addListener(
+    event: WebimEvents,
+    listener: WebimEventListener
+  ): WebimSubscription {
+    const subscription = emitter.addListener(event, listener);
+    return new WebimSubscription(() => RNWebim.removeListener(subscription));
+  }
+
+  public static removeListener(listener: EmitterSubscription): void {
+    emitter.removeSubscription(listener);
+  }
+
+  static removeAllListeners(event: WebimEvents) {
+    emitter.removeAllListeners(event);
+  }
 }
+
+export * from './types';
+export * from './utils';
+export default RNWebim;
