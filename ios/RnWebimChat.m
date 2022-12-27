1723e8641809ca1c873fb0a344359f2ae532cc91
Maxim Vasin <maxim.vasin@mcc-tomsk.de>
2022-12-27 17:23:24 +0700

    refactor: rewrote methods as extern attrbuted

--- ios/RnWebimChat.m
+++ ios/RnWebimChat.m
@@ -2,9 +2,41 @@
 
 @interface RCT_EXTERN_MODULE(RnWebimChat, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(resumeSession:(NSDictionary*)builderData
+                  withRejecter:(RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(destroySession:(nonnull NSNumber*)clearUserData
+                  withRejecter:(RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(getLastMessages:(nonnull NSNumber*)limit
+                  withRejecter:(RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(getNextMessages:(NSNumber*)limit
+                  withRejecter:(RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(getAllMessages:
+                  (RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(rateOperator:(NSNumber*)rating
+                  withRejecter:(RCTResponseSenderBlock)reject
+                  resolve:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(tryAttachFile:
+                  (RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(sendFile:(NSString*)uri withName:(NSString*)name withMime:(NSString*)mime withExtention:(NSString*)extention
+                  withRejecter:(RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
+
+RCT_EXTERN_METHOD(send:(NSString*)message
+                  withRejecter:(RCTResponseSenderBlock)reject
+                  withResolver:(RCTResponseSenderBlock)resolve)
 
 + (BOOL)requiresMainQueueSetup
 {
